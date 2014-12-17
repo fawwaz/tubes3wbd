544f416b1cea40c48d0266292cf3b9df27c06c6e
fawwazmuhammad <fawwazmuhammad@gmail.com>
2014-12-17 11:51:40 +0700

    added heroku function

--- src/main/java/com/github/fawwaz/heroku/service/Api.java
+++ src/main/java/com/github/fawwaz/heroku/service/Api.java
@@ -13,4 +13,24 @@ import javax.jws.WebService;
 public interface Api {
 
 	String sayHi(String input);
+        
+        
+        boolean createPost(String title,String content,String date);
+        boolean readPost();
+        boolean updatePost(Integer id,String judul,String content,String date);
+        boolean deletePost(Integer id);
+        boolean publishPost(Integer id);
+        boolean searchPost(String query);
+        
+        
+        boolean createUser(String name,String email,String role);
+        boolean readUser();
+        boolean updateUser(Integer id,String nama,String role,String email);
+        boolean deleteUser(Integer id);
+        
+        boolean createComment(Integer id_post,String name,String email,String content);
+        boolean readComment(Integer id_post);
+        boolean deleteComment(Integer id_comment);
+        
+        
 }
--- src/main/java/com/github/fawwaz/heroku/service/ApiImpl.java
+++ src/main/java/com/github/fawwaz/heroku/service/ApiImpl.java
@@ -8,11 +8,77 @@ package com.github.fawwaz.heroku.service;
  * 
  */
 public class ApiImpl implements Api {
+    
+    public final static String FBase_URL = "";
+    @Override
+    public boolean createPost(String title, String content, String date) {
+        return true;
+    }
 
-	@Override
-	public String sayHi(String input) {
-		System.out.println("Hello invoked : " + input);
-		return String.format("Hello '%s'", input);
-	}
+    @Override
+    public boolean readPost() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
+    @Override
+    public boolean updatePost(Integer id, String judul, String content, String date) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean deletePost(Integer id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean publishPost(Integer id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean searchPost(String query) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean createUser(String name, String email, String role) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean readUser() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean updateUser(Integer id, String nama, String role, String email) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean deleteUser(Integer id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean createComment(Integer id_post, String name, String email, String content) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean readComment(Integer id_post) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean deleteComment(Integer id_comment) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String sayHi(String input) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+        
 }
